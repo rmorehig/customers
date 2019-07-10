496baabc4b3d22c9f0e95eb59e2485289d185d29
Rafael Moreno Higueras <rmh1516@gmail.com>
2019-07-10 19:07:01 +0200

    Links react-router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import {Link, BrowserRouter as Router} from 'react-router-dom'
 import AppFrame from './components/AppFrame';
 
 function App() {
   return (
-    <div className="App">
-      <AppFrame/>
-    </div>
+    <Router>
+      <div className="App">
+        <Link to="/customers">Customers</Link>
+        <Link to="/customers/30000">Customers 30000</Link>
+      </div>
+    </Router>
   );
 }
 
