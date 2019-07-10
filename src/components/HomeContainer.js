2c2d5dbc43d9c69a9a2ef399347cc34bd03adfd1
Rafael Moreno Higueras <rmh1516@gmail.com>
2019-07-10 20:55:46 +0200

    HomeContainer

--- src/components/HomeContainer.js
+++ src/components/HomeContainer.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React,{Component} from 'react';
 import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom'
-const HomeContainer = props => {
-    return (
-        <div>
-            <h1>Home</h1>
-            <Link to="/customers">Listado de clientes</Link>
-        </div>
-    );
+import AppFrame from "./AppFrame";
+import CustomerActions from './CustomerActions';
+
+class HomeContainer extends Component {
+
+    handleOnClick = () => {
+        console.log('handle on click')
+    }
+
+    render() {
+        return (
+            <div>
+                <AppFrame 
+                    header='Home' 
+                    body={
+                        <div>
+                            Esta es la pantalla inicial
+                            <CustomerActions>
+                                <button onClick={this.handleOnClick}>Listado de clientes</button>
+                            </CustomerActions>
+                        </div>
+                    }/>
+            </div>
+        );
+            }
 };
 
 HomeContainer.propTypes = {
